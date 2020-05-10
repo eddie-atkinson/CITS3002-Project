7379dda62e6162df0842135e8275e416284c30c2
Eddie Atkinson <22487668@student.uwa.edu.au>
2020-05-10 18:44:33 +0800

    adds packet class, makes connections non-blocking, jettisons terrible library

--- src/station.py
+++ src/station.py
@@ -11,12 +11,15 @@ Code conforming to Pylinter and Mypy
 import socket
 import sys
 import os
-import selectors
+import select
 import signal
 from typing import Any
 from time import time
 from Frame import Frame
 from FrameType import FrameType
+from Packet import Packet
+from Packet import DisconnectionError
+import random
 
 
 HOST = "127.0.0.1"
@@ -29,6 +32,8 @@ UDP_PORT: int
 NEIGHBOURS: dict = {}
 TCP_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 UDP_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+TIMEOUT_IN_SECS = 5
+MAX_FAILURES = 15
 
 
 def main() -> None:
@@ -37,87 +42,135 @@ def main() -> None:
     open_ports()
 
 
+def check_lost_frames(ack_queue: list, sending_queue: dict) -> None:
+    if ack_queue:
+        packet = ack_queue[0]
+        frame_time = packet.time
+        current_time = int(time())
+        time_delta = current_time - frame_time
+        while time_delta > TIMEOUT_IN_SECS:
+            # Calculate backoff time for repeated packet or send if new packet
+            ack_queue.pop(0)
+            packet.time = int(time())
+            if packet.failures <= MAX_FAILURES:
+                packet.failures += 1
+            else:
+                raise DisconnectionError("Packet dropped 15 times")
+            backoff_time = (random.randrange(0, packet.failures)
+                            * TIMEOUT_IN_SECS)
+            if backoff_time > 0:
+                packet.time = int(time()) + backoff_time
+                ack_queue.append(packet)
+            else:
+                sending_queue[UDP_SOCKET].append(packet)
+            if ack_queue:
+                # Check the remaining frames
+                packet = ack_queue[0]
+                frame_time = packet.time
+                time_delta = current_time - frame_time
+            else:
+                # There are no more frames
+                break
+
+
 def open_ports() -> None:
-    sel = selectors.DefaultSelector()
     seqno = 0
+    input_sockets = [TCP_SOCKET, UDP_SOCKET]
+    output_sockets = [TCP_SOCKET, UDP_SOCKET]
+    ack_queue = []
+    sending_queue = {}
+    sending_queue[TCP_SOCKET] = []
+    sending_queue[UDP_SOCKET] = []
     TCP_SOCKET.bind((HOST, TCP_PORT))
     UDP_SOCKET.bind((HOST, UDP_PORT))
-    TCP_SOCKET.listen()
+
+    # Only allow a single client for now
+    TCP_SOCKET.listen(1)
 
     TCP_SOCKET.setblocking(False)
-    # UDP_SOCKET.setblocking(False)
-
-    sel.register(TCP_SOCKET, selectors.EVENT_READ, None)
-    sel.register(UDP_SOCKET, selectors.EVENT_READ, None)
+    UDP_SOCKET.setblocking(False)
 
     # Introduce ourselves to the neighbours
-    send_name_frame(seqno)
-    seqno = (seqno + 1) % MAX_INT
+    for neighbour in NEIGHBOURS.keys():
+        name_frame = Frame(NAME, "", seqno, NAME, int(time()),
+                           FrameType.NAME_FRAME)
+        sending_packet = Packet(name_frame.to_bytes(), neighbour,
+                                int(time()), 0, FrameType.NAME_FRAME)
+        sending_queue[UDP_SOCKET].append(sending_packet)
+        seqno = (seqno + 1) % MAX_INT
 
     while True:
-        events = sel.select()
-        for key, mask in events:
-            incoming_socket: Any = key.fileobj
-            if key.data is None:
-                # Should be a new connection
-                if incoming_socket == UDP_SOCKET:
-                    process_udp()
-                else:
-                    print("receving TCP connection")
-                    accept(incoming_socket, sel)
+        # Check for lost frames
+        check_lost_frames(ack_queue, sending_queue)
+
+        readers, writers, errors = select.select(input_sockets,
+                                                 output_sockets, input_sockets)
+        for reader in readers:
+            if reader == UDP_SOCKET:
+                frame = UDP_SOCKET.recvfrom(MAX_PACKET_LEN)
+                process_udp(sending_queue, ack_queue, frame)
+
+            elif reader == TCP_SOCKET:
+                conn, addr = TCP_SOCKET.accept()
+                print(f"New connection from {addr}")
+                conn.setblocking(False)
+                sending_queue[conn] = []
+                input_sockets.append(conn)
+                output_sockets.append(conn)
+
             else:
-                # Existing connection
-                service_connection(key, mask, sel)
+                # Existing TCP sockets
+                incoming_bytes = reader.recv(1024)
+                if not incoming_bytes:
+                    # Disconnection message
+                    print(f"Disconnecting from {reader}")
+                    if reader in output_sockets:
+                        output_sockets.remove(reader)
+                    input_sockets.remove(reader)
+                else:
+                    # They have something to say
+                    print(incoming_bytes.decode("utf-8"))
+
+        for writer in writers:
+            if sending_queue[writer]:
+                packet = sending_queue[writer].pop(0)
+                if writer != UDP_SOCKET:
+                    # If its a TCP Socket use send
+                    writer.send(packet.frame_bytes)
+                else:
+                    # It's UDP
+                    packet.time = int(time())
+                    if (packet.type != FrameType.ACK
+                            and packet.type != FrameType.NACK):
+                        ack_queue.append(packet)
+                    writer.sendto(packet.frame_bytes, (HOST, packet.port))
 
 
-def process_udp() -> None:
+def process_udp(sending_queue: dict, ack_queue: list,
+                frame_bytes: bytes) -> None:
+
     # Read 4 byte header giving frame length
-    frame = UDP_SOCKET.recvfrom(MAX_PACKET_LEN)
-    origin_port = frame[1][1]
-    frame_str: str = frame[0].decode("utf-8")
+    origin_port = frame_bytes[1][1]
+    frame_str: str = frame_bytes[0].decode("utf-8")
     incoming_frame = Frame()
     incoming_frame.from_string(frame_str)
+
     if incoming_frame.type == FrameType.NAME_FRAME:
         NEIGHBOURS[origin_port] = incoming_frame.origin
+        ack = Frame(incoming_frame.origin, incoming_frame.origin,
+                    incoming_frame.seqno, NAME, -1, FrameType.ACK)
+        packet = Packet(ack.to_bytes(), origin_port, -1, 0, FrameType.ACK)
+        sending_queue[UDP_SOCKET].append(packet)
 
-
-def send_name_frame(seqno: int) -> None:
-    name_frame = Frame(NAME, "", seqno, NAME, int(time()),
-                       FrameType.NAME_FRAME)
-    send_bytes = name_frame.to_bytes()
-    for neighbour in NEIGHBOURS.keys():
-        UDP_SOCKET.sendto(send_bytes, ((HOST, neighbour)))
-
-
-def accept(sock: socket.socket, sel: selectors.DefaultSelector) -> None:
-    conn, addr = sock.accept()
-    print(f"Accepting connection from {addr}")
-    conn.setblocking(False)
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events)
-
-
-def service_connection(key: selectors.SelectorKey, mask: int,
-                       sel: selectors.DefaultSelector) -> None:
-    sock: Any = key.fileobj
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)
-        if recv_data:
-            print("Received data over TCP, data :" + str(recv_data))
-            sock.send(b"HTTP/1.0 200 OK\n")
-            sock.send(b"Content-Type: text/html\n")
-            sock.send(b"\n")
-            sock.send(b"""
-                <html>
-                    <body>
-                    <h1> Hello World</h1>
-                    </body>
-                </html>
-            """)
-        else:
-            print("Closing socket")
-            sel.unregister(sock)
-            sock.close()
+    elif incoming_frame.type == FrameType.ACK:
+        for i in range(len(ack_queue)):
+            frame_str = ack_queue[i].frame_bytes.decode("utf-8")
+            frame = Frame()
+            frame.from_string(frame_str)
+            if (frame.seqno == incoming_frame.seqno
+               and incoming_frame.origin == NAME):
+                ack_queue.pop(i)
+                break
 
 
 def parse_args() -> None:
